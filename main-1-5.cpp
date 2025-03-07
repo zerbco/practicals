581d8e262eef6c29791edaf4ff77d7f9a7bf2e26
zerbco <federicoektn09@gmail.com>
2025-03-07 16:01:29 +1030

    Add files via upload

--- main-1-5.cpp
+++ main-1-5.cpp
@@ -5,6 +5,6 @@ extern int count_evens(int number);
 int main() {
     int number = 10;
 
-    std::cout << "There are " << count_evens(number) << " even numbers between number 1 and " << number << std::endl;
+    std::cout << "There are " << count_evens(number) << " even numbers"<< std::endl;
     return 0;
 }
